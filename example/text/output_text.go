2b137a37c3c7efbd140d9d49a045b498d8ed1ac2
Denis Dyakov <denis.dyakov@gmail.com>
2015-07-01 08:32:52 +0600

    Update output_text.go

--- example/text/output_text.go
+++ example/text/output_text.go
@@ -8,7 +8,10 @@ import (
 )
 
 func main() {
+	// Create new LED matrix with number of cascaded devices is equal to 1.
 	mtx := max7219.NewMatrix(1)
+	// Open SPI device with spibus and spidev equal to 0 and 0.
+	// Set brightness equal to 7.
 	err := mtx.Open(0, 0, 7)
 	if err != nil {
 		log.Fatal(err)
@@ -23,10 +26,13 @@ func main() {
 	// font = max7219.FontTiny
 	// font = max7219.FontVestaPK8000Rus
 	font = max7219.FontCP437
+	// Output text message to LED matrix.
 	mtx.SlideMessage("Hello world!!! Hey Beavis, let's rock!",
 		font, true, 50*time.Millisecond)
+	// Wait 1 sec, then continue output new text.
 	time.Sleep(1 * time.Second)
 	font = max7219.FontZXSpectrumRus
+	// Output national text to LED matrix.
 	mtx.SlideMessage("Привет мир!!! Шарик - ты балбес!!!",
 		font, true, 50*time.Millisecond)
 }
